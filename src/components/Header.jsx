dd8f5aec57efb0a5831f7b9f5e570b6db75d8fd4
Christian Bound <automaticbound@gmail.com>
2023-03-29 17:57:55 +0900

    rough layout in progress. fixed random colour generator

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -5,17 +5,16 @@ const Header = () => {
   const colourPalette = [
     "#63399B",
     "#F7F30A",
-    "#F7F30A",
-    "#F7F30A",
-    "#F7F30A",
-    "#F7F30A",
+    "#F5341B",
+    "#D76515",
+    "#2BB245",
+    "#0C7EA7",
     "#F95389",
   ];
 
-  const generateColor = colourPalette[Math.floor(Math.random() * colourPalette.length)];;
+  const generateColor =
+    colourPalette[Math.floor(Math.random() * colourPalette.length)];
   const [colour] = useState(generateColor);
-
-  console.log(colour)
 
   return (
     <header>
@@ -23,13 +22,17 @@ const Header = () => {
         width="70%"
         height="300px"
         transform="rotate(45)"
-        x="-1000"
         xmlns="http://www.w3.org/2000/svg"
         fill={colour}
         display="block"
         transform-origin="269px -139px"
       >
-        <rect width="300" height="300" x="0" y="0" />
+        <g>
+          <rect width="300" height="300" x="0" y="0" />
+          <text x="1000" y="1000" fontFamily="Verdana" fill="black" fontSize="35">
+            I love SVG!
+          </text>
+        </g>
       </svg>
     </header>
   );
