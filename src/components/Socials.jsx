f2515c1103781982bfe8bb14a03a1017c42abc44
Christian Bound <automaticbound@gmail.com>
2023-05-30 15:41:23 +0900

    added a portfolio link.

--- src/components/Socials.jsx
+++ src/components/Socials.jsx
@@ -7,11 +7,14 @@ import {
   FaEnvelope,
   FaMediumM,
 } from "react-icons/fa";
+import Pdf from "../assets/Christian_Bound_Resume.pdf";
 
 export default function Socials(props) {
   return (
     <div className="contactBox">
-      <img className="cvLink" src="/images/CVQR.png" alt="cvQRcode" />
+      <a className="cvLink" target="_blank" href={Pdf} rel="noreferrer">
+        <img className="qrCode" src="/images/CVQR.png" alt="cvQRcode"></img>
+      </a>
       <div className="contactText">
         <div
           className="socialsBox"
@@ -20,27 +23,51 @@ export default function Socials(props) {
           <h1 className="playerTitle">PLAYERS: 1</h1>
           <h1 className="peripheralTitle">PERIPHERALS:</h1>
           <div className="socialsLinks">
-            <a target="_blank" rel="noreferrer" href="https://www.linkedin.com/in/christian-bound/">
+            <a
+              target="_blank"
+              rel="noreferrer"
+              href="https://www.linkedin.com/in/christian-bound/"
+            >
               <FaLinkedin className="socialIcon" />
               <span className="tooltiptext">LinkedIn</span>
             </a>
-            <a target="_blank" rel="noreferrer" href="https://github.com/cpbound">
+            <a
+              target="_blank"
+              rel="noreferrer"
+              href="https://github.com/cpbound"
+            >
               <FaGithubAlt className="socialIcon" />
               <span className="tooltiptext">GitHub</span>
             </a>
-            <a target="_blank" rel="noreferrer" href="https://medium.com/@cpbound87">
+            <a
+              target="_blank"
+              rel="noreferrer"
+              href="https://medium.com/@cpbound87"
+            >
               <FaMediumM className="socialIcon" />
               <span className="tooltiptext">Medium</span>
             </a>
-            <a target="_blank" rel="noreferrer" href="https://soundcloud.com/adult_time">
+            <a
+              target="_blank"
+              rel="noreferrer"
+              href="https://soundcloud.com/adult_time"
+            >
               <FaSoundcloud className="socialIcon" />
               <span className="tooltiptext">Soundcloud</span>
             </a>
-            <a target="_blank" rel="noreferrer" href="https://adulttime.bandcamp.com/">
+            <a
+              target="_blank"
+              rel="noreferrer"
+              href="https://adulttime.bandcamp.com/"
+            >
               <FaBandcamp className="socialIcon" />
               <span className="tooltiptext">Bandcamp</span>
             </a>
-            <a target="_blank" rel="noreferrer" href="mailto:cpbound87@gmail.com">
+            <a
+              target="_blank"
+              rel="noreferrer"
+              href="mailto:cpbound87@gmail.com"
+            >
               <FaEnvelope className="socialIcon" />
               <span className="tooltiptext">Mail me</span>
             </a>
@@ -50,7 +77,11 @@ export default function Socials(props) {
           <div className="footerText">
             2023 © Christian Bound {"\n"} Made In Japan 2023-05-01 {"\n"} Sega
             logo style font from{" "}
-            <a target="_blank" rel="noreferrer" href="https://actselect.chips.jp/">
+            <a
+              target="_blank"
+              rel="noreferrer"
+              href="https://actselect.chips.jp/"
+            >
               https://actselect.chips.jp/
             </a>
           </div>
