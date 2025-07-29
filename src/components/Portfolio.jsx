ac70fd0ce36d002086090d423db1a52e91654b68
Christian Bound <automaticbound@gmail.com>
2025-07-29 13:40:15 +0900

    thumbnail and projects title updated

--- src/components/Portfolio.jsx
+++ src/components/Portfolio.jsx
@@ -8,31 +8,43 @@ const Portfolio = () => {
     const media = window.innerWidth;
     if (media > query) {
       return (
-        <div className="projects">
-          <div className="projectsBox">
-            {portfolio.map((port, idx) => {
-              return <Modal key={idx} portfolio={port} />;
-            })}
+        <div className="portfolioContainer">
+          <h1 className="projectsHeader">⬇️Projects⬇️</h1>
+          <div className="projects">
+            <div className="projectsBox">
+              {portfolio.map((port, idx) => {
+                return <Modal key={idx} portfolio={port} />;
+              })}
+            </div>
           </div>
         </div>
       );
     } else {
       return (
         <div className="projectsBox">
-          {portfolio.map((port, idx) =>  {
+          {portfolio.map((port, idx) => {
             return (
-                <div key={idx} id={idx + 1} className="projectsModal" style={idx % 2 === 0 ? { backgroundColor: 'whitesmoke', borderRadius: 16} : {}}>
+              <div
+                key={idx}
+                id={idx + 1}
+                className="projectsModal"
+                style={
+                  idx % 2 === 0
+                    ? { backgroundColor: "whitesmoke", borderRadius: 16 }
+                    : {}
+                }
+              >
                 <a target="_blank" rel="noreferrer" href={port.url}>
-                <img
-                className="projectThumbnail"
-                src={port.cover}
-                alt="portfolio"
-                />
+                  <img
+                    className="projectThumbnail"
+                    src={port.cover}
+                    alt="portfolio"
+                  />
                 </a>
                 <h1 className="projectHeader">{port.title}</h1>
                 <p className="projectDescription">{port.description}</p>
-                </div>
-                );
+              </div>
+            );
           })}
         </div>
       );
